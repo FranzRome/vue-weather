f1636e120f4ec78cb14a6bb804d39871c1239fb8
Franz <francesco.romeo-work@libero.it>
2021-08-11 13:03:06 +0200

    New update
    New API key, added kelvin to celsius conversion

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="temp">{{ Math.round(kelvinToCelsius(weather.main.temp)) }}°c</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
@@ -32,7 +32,7 @@ export default {
   name: 'app',
   data () {
     return {
-      api_key: 'f6a2c317f97367bfe8fa3e5fc517e80d',
+      api_key: '77f423df329515b94c176383b1543438',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
@@ -41,7 +41,7 @@ export default {
   methods: {
     fetchWeather (e) {
       if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        fetch(`${this.url_base}weather?q=${this.query}&appid=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
@@ -59,6 +59,9 @@ export default {
       let month = months[d.getMonth()];
       let year = d.getFullYear();
       return `${day} ${date} ${month} ${year}`;
+    },
+    kelvinToCelsius(kelvin){
+      return kelvin - 273.15
     }
   }
 }
